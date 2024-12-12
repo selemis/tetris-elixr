84c68845a20425b69dc4dd76f7e51596dcb61957
Stelios Omirou <omirous@interamerican.gr>
2024-12-12 23:42:05 +0200

    rotate points 90 degrees

--- lib/points.ex
+++ lib/points.ex
@@ -6,4 +6,18 @@ defmodule Tetris.Points do
       |> Enum.map( fn {px, py} -> {px + x, py + y} end)
   end
 
+  def rotate_90(points), do: points |> transpose |> mirror_on_y()
+
+  def transpose(points), do: points |> Enum.map(&transpose_point/1)
+
+  def mirror_on_y(points), do: points |> Enum.map(&mirror_point_on_y/1)
+
+  def mirror_on_x(points), do: points |> Enum.map(&mirror_point_on_x/1)
+
+  defp transpose_point({x, y}), do: {y, x}
+
+  defp mirror_point_on_y({x, y}), do: {4 - x, y}
+
+  defp mirror_point_on_x({x, y}), do: {x, 4 - y}
+
 end
